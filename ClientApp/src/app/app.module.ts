61b1a348b13c4dbf1c2028445ec00b844a97d3a3
Reyhani Shalmani, Omid <O.Reyhani-Shalmani@iaea.org>
2018-06-29 17:30:29 +0200

    update version

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,9 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { CustomMaterialModule } from './CustomMaterialModule';
+import { LoginComponent } from './login/login.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
@@ -16,15 +19,20 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    LoginComponent,
+    UserComponent
   ],
   imports: [
+    CustomMaterialModule,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'dfd', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
+      { path: '', component: LoginComponent, pathMatch:'full' },
+      { path: 'user', component: UserComponent },
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
